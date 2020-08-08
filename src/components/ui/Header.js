cc36164f144dc94ca81dbafef71587054c9851c1
Haiyang Hong <hong961127@gmail.com>
2020-08-08 11:51:14 -0700

    Add tab and tabs on header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,14 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+
 import AppBar from "@material-ui/core/Appbar";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/styles";
+
+import logo from "../../assets/logo.svg";
 
 const ElevationScroll = ({ children }) => {
   const trigger = useScrollTrigger({
@@ -19,18 +24,62 @@ const ElevationScroll = ({ children }) => {
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar, // To push content below AppBar
+    marginBottom: "3rem", // Offset added logo height
+  },
+  logo: {
+    height: "7rem",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: 25,
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    marginLeft: 50,
+    marginRight: 25,
+    height: 45,
   },
 }));
 
 const Header = () => {
+  const [value, setValue] = useState(0);
+
   const classes = useStyles();
+
+  const handleChange = (evt, value) => {
+    setValue(value); // Setup click change tab, evt is required
+  };
 
   return (
     <Fragment>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
-          <Toolbar>
-            <Typography variant="h3">Arc Development</Typography>
+          <Toolbar disableGutters>
+            <img src={logo} alt="logo" className={classes.logo} />
+            <Tabs
+              value={value}
+              className={classes.tabContainer}
+              onChange={handleChange}
+              indicatorColor="primary"
+            >
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
