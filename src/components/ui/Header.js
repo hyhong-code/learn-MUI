be4b5658b5ff29036515fb2098995d0f9b179034
Haiyang Hong <hong961127@gmail.com>
2020-08-08 14:47:28 -0700

    Finish header menu

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -5,6 +5,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/styles";
 import { Link } from "react-router-dom";
@@ -51,31 +53,109 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "transparent",
     },
   },
+  menu: {
+    backgroundColor: theme.palette.common.blue,
+    color: "#fff",
+    borderRadius: 0,
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1,
+    },
+  },
 }));
 
 const Header = () => {
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const classes = useStyles();
 
   useEffect(() => {
-    if (window.location.pathname === "/" && value !== 0) {
-      setValue(0);
-    } else if (window.location.pathname === "/services" && value !== 1) {
-      setValue(1);
-    } else if (window.location.pathname === "/revolution" && value !== 2) {
-      setValue(2);
-    } else if (window.location.pathname === "/about" && value !== 3) {
-      setValue(3);
-    } else if (window.location.pathname === "/contact" && value !== 4) {
-      setValue(4);
-    } else if (window.location.pathname === "/estimate" && value !== 5) {
-      setValue(5);
+    switch (window.location.pathname) {
+      case "/":
+        if (value !== 0) {
+          setValue(0);
+        }
+        break;
+      case "/services":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(0);
+        }
+        break;
+      case "/customsoftware":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+      case "/mobileapps":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(2);
+        }
+        break;
+      case "/websites":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(3);
+        }
+        break;
+      case "/revolution":
+        if (value !== 2) {
+          setValue(2);
+        }
+        break;
+      case "/about":
+        if (value !== 3) {
+          setValue(3);
+        }
+        break;
+      case "/contact":
+        if (value !== 4) {
+          setValue(4);
+        }
+        break;
+      case "/estimate":
+        if (value !== 5) {
+          setValue(5);
+        }
+        break;
+      default:
+        break;
     }
-  }, [value]);
+  }, []);
 
   const handleChange = (evt, value) => {
     setValue(value); // Setup click change tab, evt is required
   };
+
+  const handleClick = (evt) => {
+    setAnchorEl(evt.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (evt) => {
+    setAnchorEl(null);
+    setOpen(false);
+  };
+
+  const handleMenuItemClick = (evt, idx) => {
+    setAnchorEl(null);
+    setOpen(false);
+    setSelectedIndex(idx);
+  };
+
+  const menuOptions = [
+    { name: "Services", link: "/services" },
+    { name: "Custom Software Development", link: "/customsoftware" },
+    { name: "Mobile App Development", link: "/mobileapps" },
+    { name: "Website Development", link: "/websites" },
+  ];
 
   return (
     <Fragment>
@@ -104,6 +184,9 @@ const Header = () => {
                 label="Home"
               />
               <Tab
+                aria-owns={anchorEl ? "service-menu" : undefined}
+                aria-haspopup={anchorEl ? "true" : undefined}
+                onMouseOver={(evt) => handleClick(evt)}
                 className={classes.tab}
                 component={Link}
                 to="/services"
@@ -135,6 +218,31 @@ const Header = () => {
             >
               Free Estimate
             </Button>
+            <Menu
+              id="service-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              classes={{ paper: classes.menu }}
+              elevation={0}
+              MenuListProps={{ onMouseLeave: handleClose }} // Props applied to the MenuList element.
+            >
+              {menuOptions.map((option, idx) => (
+                <MenuItem
+                  key={idx}
+                  onClick={(evt) => {
+                    handleMenuItemClick(evt, idx);
+                    setValue(1);
+                  }}
+                  component={Link}
+                  to={option.link}
+                  classes={{ root: classes.menuItem }}
+                  selected={idx === selectedIndex && value === 1}
+                >
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
